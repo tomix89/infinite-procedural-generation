d820c4e05ab88524cfc1b6d7700ce715587555fe
tomix89 <tomix89@users.noreply.github.com>
2023-02-27 21:05:03 +0100

    All needed data are now in the .h file
    - tiles were moved to the generator
    - TODO make the output c# compatible and use it instead of tiles in TileTest

--- TilePacker/Program.cs
+++ TilePacker/Program.cs
@@ -9,24 +9,31 @@ using System.Threading.Tasks;
 namespace TilePacker {
     class Program {
 
+        enum BuildType { 
+        C_PP,
+        C_SHARP,
+        }
+
         // this was only ever run from the editor
-        static readonly string TILE_FOLDER_PATH = @"..\..\..\TileTest\tiles";
+        static readonly string TILE_FOLDER_PATH = @"tiles";
         static List<ushort> palette = new List<ushort>();
         static Dictionary<ushort, byte> usedColors = new Dictionary<ushort, byte>();
         static List<StringBuilder> imageCodedList = new List<StringBuilder>();
-        static List<string> imageEnumsList = new List<string>();
+        static List<string> imageNamesList = new List<string>();
+        static readonly string INDENT = "    ";
 
         static void Main(string[] args) {
 
             // Process the png files
-            string[] fileEntries = Directory.GetFiles(TILE_FOLDER_PATH);
+            string[] fileEntries = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + TILE_FOLDER_PATH);
             foreach (string fileName in fileEntries)
                 if (fileName.EndsWith(".png")) {
                     ProcessFile(fileName);
                 }
 
             writeFinalFile();
-
+            Console.WriteLine("\nDONE, press any key to exit");
+            Console.ReadKey();
         }
 
         // we need a RGB 565 format
@@ -44,7 +51,7 @@ namespace TilePacker {
 
         static void ProcessFile(string filename) {
 
-            Console.WriteLine("Procesing: " + filename);
+            Console.WriteLine("Processing: " + filename);
             Bitmap bitmap = new Bitmap(filename);
             int w = bitmap.Width;
             int h = bitmap.Height;
@@ -52,11 +59,11 @@ namespace TilePacker {
             StringBuilder sb = new StringBuilder();
 
             string imgName = Path.GetFileNameWithoutExtension(filename);
-            imageEnumsList.Add(imgName.ToUpper());
+            imageNamesList.Add(imgName);
 
             sb.Append("const uint8 " + imgName + "[] = {\n");
 
-            // since we will draw lines on the ESP32 in vertical manner, we will code them like this
+            // since we will draw lines on the ESP32 in vertical mode, we will code them like this
             for (int x = 0; x < w; ++x) {
                 for (int y = 0; y < h; ++y) {
                     ushort color565 = ColorTo565(bitmap.GetPixel(x, y));
@@ -79,7 +86,7 @@ namespace TilePacker {
 
 
                     if (cntr == 0) {
-                        sb.Append("0x" + indexed.ToString("X2"));
+                        sb.Append(INDENT + "0x" + indexed.ToString("X2"));
                     } else {
                         sb.Append(", 0x" + indexed.ToString("X2"));
                     }
@@ -107,8 +114,6 @@ namespace TilePacker {
 
         static void writeFinalFile() {
             // first all the images
-
-           
             var sw = new StreamWriter("tiles.h", false);
 
             for (int i = 0; i < imageCodedList.Count; ++i) {
@@ -116,19 +121,69 @@ namespace TilePacker {
                 sw.Write("\n\n");
             }
 
-            // write out array
+            // write out array of images (foe easy enum indexing)
+            sw.WriteLine("const uint8* allTiles[] = {");
+            for (int i = 0; i < imageNamesList.Count; ++i) {
+                if (i == 0) {
+                    sw.Write(INDENT + imageNamesList[i]);
+                } else {
+                    sw.Write(",\n" + INDENT + imageNamesList[i]);
+                }
+            }
+            sw.Write("\n}");
 
 
+
+            sw.Write("\n\n");
             Console.WriteLine("Total palette colors: " + palette.Count);
             // write out palette 
+            int cntr = 0;
+            sw.WriteLine("// in reality only " + palette.Count + " is used");
+            sw.WriteLine("const uint16 palette[] = {");
 
+
+
+            for (int i = 0; i <= 0xFF; ++i) {
+                UInt16 num = 0;
+                if ( i < palette.Count) {
+                    num = palette[i];
+                }
+
+                if (cntr == 0) {
+                    sw.Write(INDENT + "0x" + num.ToString("X4"));
+                } else {
+                    sw.Write(", 0x" + num.ToString("X4"));
+                }
+
+                cntr++;
+                // newline each 16 byte(text) 
+                if (cntr >= 8) {
+
+                    if (i < 255) {
+                        sw.Write(",\n");
+                    } else {
+                        sw.Write("\n}"); // last one closes
+                    }
+
+                    cntr = 0;
+                }
+            }
+
+
+
+            sw.Write("\n\n");
             // write out enums
+            sw.WriteLine("enum ImageNames = {");
+            for (int i = 0; i < imageNamesList.Count; ++i) {
+                if (i == 0) {
+                    sw.Write(INDENT + imageNamesList[i].ToUpper() + "=0");
+                } else {
+                    sw.Write(",\n" + INDENT + imageNamesList[i].ToUpper());
+                }
+            }
 
-            //
-
+            sw.WriteLine("\n}");
             sw.Close();
-
-
         }
 
 
