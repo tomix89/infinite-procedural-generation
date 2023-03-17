3a043222c25a98b1df8377054d2b3c333d54ab4d
tomix89 <tomix89@users.noreply.github.com>
2023-03-17 12:49:07 +0100

    Optimization
    - actually we don't even need the Hackers-Delight solution
    - we can count directly, as we have full control of all additions

--- TileTest/cHashSet.cs
+++ TileTest/cHashSet.cs
@@ -5,9 +5,10 @@ namespace TileTest {
     // since in esp32 we wont have fancy hashset, we implement ours
     // this is really trivial and optimized only for continuous enum numbers (e.g. no gaps and jumps)
     class cHashSet {
-        // as of now we have 31 tiles, so they will fit into 1 32b value
 
+        // as of now we have 37 tiles, so they will fit into 2 32b value
         private const int STORAGE_SIZE = 2;
+        private int itemCount = 0;
         private UInt32[] storage = new UInt32[STORAGE_SIZE];
 
 
@@ -21,11 +22,20 @@ namespace TileTest {
             for (int id = 0; id < STORAGE_SIZE; ++id) {
                 storage[id] = 0;
             }
+            itemCount = 0;
         }
 
         public void add(int value) {
             int id = value / 32;
-            storage[id] |= (1U << (value % 32));
+            UInt32 bit = 1U << (value % 32);
+
+            // increase count only when we haven't had this bit
+            if ((storage[id] & bit) != 0) {
+                return;
+            }
+
+            itemCount++;
+            storage[id] |= bit;
         }
 
         public bool contains(int value) {
@@ -33,27 +43,8 @@ namespace TileTest {
             return (storage[id] & (1U << (value % 32))) > 0;
         }
 
-        public UInt32 count() {
-            UInt32 count = 0;
-            for (int id = 0; id < STORAGE_SIZE; ++id) {
-                count += populationCount(storage[id]);
-            }
-            return count;
+        public int count() {
+            return itemCount;
         }
-
-        // https://github.com/hcs0/Hackers-Delight/blob/master/popArrayHS.c.txt
-        private UInt32 populationCount(UInt32 x) {
-            x = x - ((x >> 1) & 0x55555555);
-            x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
-            x = (x + (x >> 4)) & 0x0F0F0F0F;
-            x = x + (x >> 8);
-            x = x + (x >> 16);
-            return x & 0x0000003F;
-        }
-        /* Note: an alternative to the last three executable lines above is:
-           return x*0x01010101 >> 24;
-        if your machine has a fast multiplier (suggested by Jari Kirma). */
-
     }
-
 }
